962d3a1930de63d2db1005ef943dd4a424aaefb9
herdeybayor <shereefdeen4real@gmail.com>
2023-06-21 03:25:16 +0100

    update header file

--- monty.h
+++ monty.h
@@ -1,11 +1,20 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Structures */
 /**
- * stuct stack_s - doubly linked list representation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: doubly linked list node structure.
+ * 			It is used for stack (or queue)
  */
 typedef struct stack_s
 {
@@ -15,15 +24,24 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * stuct instruction_s - opcode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
+ * 			for stack (or queue)
  */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/* Global variables */
+
+/* Prototypes */
+void (*get_op_func(char *s))(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+#endif /* MONTY_H */
